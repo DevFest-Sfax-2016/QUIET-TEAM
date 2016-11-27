fc9c53e715faff7667ad6498b7024156d54be4c2
ramzi <ramzi.trabelsi@it-skills.tn>
2016-11-27 12:45:29 +0100

    second project

---
+++ app/src/main/java/com/it_skills/ramzi/mfamily2/SaveSettings.java
@@ -0,0 +1,38 @@
+package com.it_skills.ramzi.mfamily2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+
+
+public class SaveSettings {
+    public  static String UserID="";
+
+    Context context;
+    SharedPreferences ShredRef;
+    public  SaveSettings(Context context){
+        this.context=context;
+        ShredRef=context.getSharedPreferences("myRef",Context.MODE_PRIVATE);
+    }
+
+    void SaveData(String UserID){
+
+        SharedPreferences.Editor editor=ShredRef.edit();
+        editor.putString("UserID",UserID);
+
+         editor.commit();
+        LoadData();
+    }
+
+    void LoadData(){
+        UserID= ShredRef.getString("UserID","0");
+        if (UserID.equals("0")){
+
+            Intent intent=new Intent(context, Login.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/it_skills/ramzi/mfamily2/SearchType.java
@@ -0,0 +1,9 @@
+package com.it_skills.ramzi.mfamily2;
+
+
+
+public class SearchType {
+    public static int MyFollowing=1;
+    public static int OnePerson=2;
+    public static int SearchIn=3;
+}
