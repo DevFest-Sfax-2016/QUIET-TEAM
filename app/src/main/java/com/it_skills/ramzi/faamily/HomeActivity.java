aa10ebc6e70686fe2bbcc5ce3522f6d3c8f22fb2
ramzi <ramzi.trabelsi@it-skills.tn>
2016-11-27 02:30:49 +0100

    RT:second commit

--- app/src/main/java/com/it_skills/ramzi/faamily/HomeActivity.java
+++ app/src/main/java/com/it_skills/ramzi/faamily/HomeActivity.java
@@ -1,13 +1,39 @@
 package com.it_skills.ramzi.faamily;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button signoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        signoutButton = (Button) findViewById(R.id.buttonsigout);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, login.class));
+        finish();
+
     }
 }
